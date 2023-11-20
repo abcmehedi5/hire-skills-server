f29ad9b5eb755af056c045317096bfe8bc1ae856
abcmehedi5 <abcmehedi5@gmail.com>
2023-11-21 00:11:45 +0600

    delete the un used file

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const appRouter = express.Router();
-const authenticationRouter = require("./authentication_routes");
 const blogRouter = require("./Blog/blog_routes");
 
-appRouter.use(authenticationRouter);
 appRouter.use(blogRouter);
 
 module.exports = appRouter;
